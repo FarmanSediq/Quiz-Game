fdb40f1663b21ba1c96f8bf2514929c1e56f7058
kurdo1o <158173388+kurdo1o@users.noreply.github.com>
2024-02-05 20:59:34 +0100

    Update

--- QuizGame.py
+++ QuizGame.py
@@ -3,4 +3,25 @@ print("Welcome to my Quiz!")
 playing = input("Do you want to play my Quiz? ")
 
 if playing != "yes":
+    print("Goodbye!")
     quit()
+
+print("Awesome! Let's start :)")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
